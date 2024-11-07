9b9d231e7214a51d03bf93364cd19e01c295777b
Rodrigo <rodrigodelgadoo.005@gmail.com>
2024-11-07 19:45:40 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contenido}>
+        <Text style={styles.text}>HELLO WORLD</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +15,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  contenido: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000',
   },
 });
